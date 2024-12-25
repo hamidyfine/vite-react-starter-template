2e5829e89230eb4291eeab9d896a99498f172335
Hamid Yaftian <hamid.yaftian@gmail.com>
2024-12-25 18:13:23 +0330

    feat: setup store using zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { Trans, TransMacro } from './components';
+import { useAppStores } from './stores/app.store';
 
 const App = () => {
+    const bears = useAppStores((state) => state.bears);
+    const increasePopulation = useAppStores((state) => state.increasePopulation);
+
     return (
         <>
+            <button
+                type="button"
+                onClick={increasePopulation}
+            >
+                one up
+            </button>
+            <h1>🐻 {bears}</h1>
             <p>
                 <TransMacro id="tgb">Edit <code>src/App.tsx</code> and save to test HMR</TransMacro>
             </p>
@@ -11,15 +22,6 @@ const App = () => {
                     components={{ 0: <code /> }}
                     id="tgb"
                 />
-            </p>
-            <p>
-                <TransMacro id="test">New Trans to test watcher!</TransMacro>
-            </p>
-            <p>
-                <TransMacro>Another New Trans to test watcher!</TransMacro>
-            </p>
-            <p>
-                <Trans id="test" />
             </p>
         </>
     );
