a417ed3eff09f836d48db14b0e61a4b0ce7c08c5
Hamid Yaftian <hamid.yaftian@gmail.com>
2024-12-27 01:08:49 +0330

    feat: setup icon package and icon component

--- src/pages/index.lazy.tsx
+++ src/pages/index.lazy.tsx
@@ -1,5 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 
+import { Icon } from '@/components';
+
 export const Route = createLazyFileRoute('/')({
     component: Index,
 });
@@ -8,6 +10,11 @@ function Index() {
     return (
         <div className="p-2">
             <h3>Welcome Home!</h3>
+            <Icon
+                name="house"
+                size={24}
+                strokeWidth={1.5}
+            />
         </div>
     );
 }
