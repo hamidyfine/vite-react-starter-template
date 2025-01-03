318e581f812e0831274c79732ca3334a8dcdd0e0
Hamid Yaftian <hamid.yaftian@gmail.com>
2025-01-03 14:23:04 +0330

    feat: add theme configuration and font assets

--- src/providers/theme/theme.provider.tsx
+++ src/providers/theme/theme.provider.tsx
@@ -3,9 +3,14 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import type { ComponentProps, PropsWithChildren } from 'react';
 
+import { theme } from '@/configs';
+
 export const ThemeProvider = ({ children, ...props }: PropsWithChildren<ComponentProps<typeof MantineProvider>>) => {
     return (
-        <MantineProvider {...props}>
+        <MantineProvider
+            {...props}
+            theme={theme}
+        >
             {children}
         </MantineProvider>
     );
